1584cc4e8859f513a849b1414d628ace6678d687
Kaiser Dandangi <kdandangi@freshbooks.com>
2017-03-16 19:40:25 -0400

    Refactored extension to use google finance. Google finance allows you to query other exchanges outside of US like TSE.

--- popup.js
+++ popup.js
@@ -1,29 +1,17 @@
 // Copyright (c) 2016 Kaiser Dandangi. All rights reserved.
-
 function getQuotes (quotes, callback, errorCallback, status) {
   if (!quotes) quotes = ["TSLA","VOO","BOX","MSFT","BBRY","NVDA","CGC","F"];// ["YHOO","AAPL","GOOG","MSFT"];
-  var searchUrl = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(" +
-    // encodeURIComponent("\"YHOO\",\"AAPL\",\"GOOG\",\"MSFT\"") + 
-    encodeURIComponent("\"" + quotes.join("\",\"") + "\"") + 
-    ")%0A%09%09&format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env&callback=";
+  
+  var searchUrl = "https://finance.google.com/finance/info?client=ig&q="
+  searchUrl += encodeURIComponent(quotes.join(","));
   
   var x = new XMLHttpRequest();
   x.open('GET', searchUrl);
-  x.responseType = 'json';
   x.onload = function() {
     var response = x.response;
-    if (!response || !response.query || !response.query.results || !response.query.results.quote) {
-      errorCallback('No response from Yahoo Query search!');
-      return;
-    }
+    
     status("Success!");
-    // console.log(response.query.results.quote);
-    // Special case in case of only searching for one stock
-    // quote is the response for that pick, rather than it being an array
-    response = response.query.results.quote;
-    if (!response.length) response = [response];
-
-    callback(response);
+    callback(JSON.parse(response.slice(4)));
   };
   x.onerror = function() {
     errorCallback('Network error.');
@@ -34,7 +22,6 @@ function getQuotes (quotes, callback, errorCallback, status) {
 
 function renderStatus (statusText) {
   document.getElementById('status').textContent = statusText;
-  // chrome.extension.getBackgroundPage().console.log(statusText);
 }
 
 function renderStockPicks (stockPicks, element) {
@@ -43,7 +30,19 @@ function renderStockPicks (stockPicks, element) {
   element.textContent = "Your picks: " + stockPicks.join(", ")
 }
 
+function normalizeQuotes (quotes) {
+  return quotes.map((quote) => {
+    return {
+      symbol: quote.t,
+      LastTradePriceOnly: quote.l,
+      ChangeinPercent: quote.c
+    };
+  });
+}
+
 function renderQuotes (quotes) {
+  // Normalize the google finance response to be the same as yql
+  quotes = normalizeQuotes(quotes);
   // alert("Rendering", JSON.strigify(quote));
   // document.getElementById('status').textContent = "RENDERED"; // quote.LastTradePriceOnly;
   // document.getElementById('current').innerHTML = "RENDERED!";// quote.LastTradePriceOnly;
@@ -70,7 +69,6 @@ function renderQuotes (quotes) {
 }
 
 function removeStockPick (quote) {
-  console.log("remove", quote);
   var stockPicks = JSON.parse(localStorage.stockPicks);
   if (stockPicks.includes(quote)) {
     stockPicks.splice(stockPicks.indexOf(quote), 1);
@@ -110,7 +108,6 @@ document.addEventListener('DOMContentLoaded', function() {
   } else {
     stockPicks = JSON.parse(stockPicks);
   }
-  // console.log(stockPicks);
   renderStockPicks(stockPicks);
   getQuotes(stockPicks, renderQuotes, renderStatus, renderStatus);
 
